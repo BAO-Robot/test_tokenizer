79da094c69c372767b832df71f7334718391b071
SPFA <2997839760@qq.com>
2024-01-07 23:34:25 +0800

    测试相关代码
    测试 tokenizer、embedding，下一步测试 word2vec 的训练

---
+++ utils/configure_util.py
@@ -0,0 +1,34 @@
+import yaml
+
+
+class ConfLoader:
+    """用于加载配置文件，并进行交互
+
+    Attributes:
+        params (Dict): 报存配置文件的参数
+    """
+    def __init__(self, path):
+        self.params = self._read(path)
+
+    @staticmethod
+    def _read(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            dct = yaml.load(f.read(), Loader=yaml.FullLoader)
+        return dct
+
+    def _save(self, path):
+        with open(path, 'w', encoding='utf-8') as f:
+            yaml.dump(self.params, stream=f, allow_unicode=True)
+
+    def add(self, **kwargs):
+        for k, v in kwargs.items():
+            self.params[k] = v
+
+    def __str__(self):
+        info = ""
+        for k, v in self.params.items():
+            info = info + f"[PARAM] {k}: {v}\n"
+        return info
+
+    def __getitem__(self, key):
+        return self.params[key]
